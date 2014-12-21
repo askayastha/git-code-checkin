b5e41ddb904090c35f8dbafb42a4fe3f9b94b736
Ashish Kayastha <ashishk.48@gmail.com>
2014-12-21 23:55:45 +0545

    Added print statements in install script to inform user what's happening.

--- install_git_code_checkin.sh
+++ install_git_code_checkin.sh
@@ -2,5 +2,8 @@
 
 sudo apt-get install xclip
 sudo mkdir -p /usr/local/bin/python_scripts/
+echo "Directory 'python_scripts' created in '/usr/local/bin'."
 sudo cp git_code_checkin.py /usr/local/bin/python_scripts/
+echo "Copied 'git_code_checkin.py' to '/usr/local/bin/python_scripts/'."
 sudo ln -s /usr/local/bin/python_scripts/git_code_checkin.py /usr/local/bin/git-code-checkin
+echo "Created a symbolic link 'git-code-checkin'."
