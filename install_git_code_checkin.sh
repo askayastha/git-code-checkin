d89ecdb0891e5848553685d5c0bc30851d8dbea3
Ashish Kayastha <ashishk.48@gmail.com>
2014-12-21 23:32:47 +0545

    Command to install xclip added to install_git_code_checkin.sh

--- install_git_code_checkin.sh
+++ install_git_code_checkin.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt-get install xclip
 sudo mkdir -p /usr/local/bin/python_scripts/
 sudo cp git_code_checkin.py /usr/local/bin/python_scripts/
 sudo ln -s /usr/local/bin/python_scripts/git_code_checkin.py /usr/local/bin/git-code-checkin
